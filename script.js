d76f81736908a4752d3a6c5ef449d04a40c8707b
lodenrogue <phyzix5761@gmail.com>
2026-03-28 09:42:25 +0800

    Fixed semantic html and styling

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let playerHTML = '';
 
 for (let i = 1; i <= 9; i++) {
   playerHTML += `
-    <player id="player-${i}">
-      <label>Player ${i}</label>
-      <button>RD</button>
-      <input type="text" class="input-hand">
-      <input type="text" class="output-equity" readonly>
+    <player>
+        <label>Player ${i}</label>
+        <button>RD</button>
+        <input type="text" class="input-hand">
+        <input type="text" class="output-equity" readonly>
     </player>`;
 }
 
-playerList.innerHTML += playerHTML;
+playerList.innerHTML = playerHTML;
